fe8c96fe1bde1a18327318f08ebe402ccf8b2c8b
keenechin <keene.chin@gmail.com>
2019-06-28 14:49:25 -0400

    magnetometer works, now for servo

---
+++ finger_controller/finger_controller.ino
@@ -0,0 +1,68 @@
+/***************************
+ * AXSimpleTest
+ * This sketch sends positional commands to the AX servo 
+ * attached to it - the servo must set to ID # 1
+ * The sketch will send a value, i, to the servo.
+ * 'For' loops are used to increment and decrement the value of 'i'
+ ***************************/
+
+//import ax12 library to send DYNAMIXEL commands
+#include <ax12.h>
+
+int current = 512;
+int point_one = 512-400;
+int point_two = 512-400;
+
+void setup()
+{
+    SetPosition(1,current); //set the position of servo # 1 to '0'
+
+    delay(100);//wait for servo to move
+    Serial.begin(9600);
+}
+
+void loop()
+{
+   
+  /*
+  //increment from 0 to 1023
+  for(int i=0;i<1024;i++)
+  {
+    SetPosition(1,i); //set the position of servo #1 to the current value of 'i'
+    delay(10);//wait for servo to move
+  }
+  //decrement from 1024 to 0
+  for(int i=1024;i>0;i--)
+  {
+    SetPosition(1,i);//set the position of servo #1 to the current value of 'i'
+    delay(10);//wait for servo to move
+  }
+  */
+ 
+
+  goPos(point_one,1);
+  //delay(500);
+  goPos(point_two,1);
+  //delay(500);
+
+}
+
+void goPos(int pos,int vel){
+  if (current < pos){
+    for(int i=current; i<=pos; i+=vel){
+      SetPosition(1,i);
+      current = i;
+      delay(10);
+      Serial.println(current);
+    }
+  }
+  
+  if(current > pos){
+    for(int i=current; i>=pos; i-=vel){
+      SetPosition(1,i);
+      current = i;
+      delay(10);
+      Serial.println(current);
+    }
+  }
+}
---
+++ magnetometer_stream/magnetometer_stream.ino
@@ -0,0 +1,57 @@
+/*
+  MLX90393 Magnetometer Example Code
+  By: Nathan Seidle
+  SparkFun Electronics
+  Date: February 6th, 2017
+  License: This code is public domain but you buy me a beer if you use this and we meet someday (Beerware license).
+
+  Read the mag fields on three XYZ axis
+
+  Hardware Connections (Breakoutboard to Arduino):
+  3.3V = 3.3V
+  GND = GND
+  SDA = A4
+  SCL = A5
+
+  Serial.print it out at 9600 baud to serial monitor.
+*/
+
+#include <Wire.h>
+#include <MLX90393.h> //From https://github.com/tedyapo/arduino-MLX90393 by Theodore Yapo
+
+MLX90393 mlx;
+MLX90393::txyz data; //Create a structure, called data, of four floats (t, x, y, and z)
+
+void setup()
+{
+  Serial.begin(9600);
+  Wire.begin();
+  mlx.begin(); //Assumes I2C jumpers are GND. No DRDY pin used.
+  mlx.setOverSampling(0);
+  mlx.setDigitalFiltering(0);
+}
+
+void loop()
+{
+  mlx.readData(data); //Read the values from the sensor
+
+
+  //Serial.print("magX[");
+  Serial.print(int(data.x));
+  Serial.print("\t");
+  //Serial.print("]\t magY[");
+  Serial.print(int(data.y));
+  Serial.print("\t");
+  //Serial.print("]\t magZ[");
+  Serial.print(int(data.z));
+  //Serial.print("] temperature(C)[");
+  //Serial.print(data.t);
+  //Serial.print("]");
+
+  Serial.println();
+
+
+  //Serial.println(int(data.y));
+
+  delay(50);
+}
