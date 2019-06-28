f9b8912c952820b607f0979dc08c7ae6ac701fe9
keenechin <keene.chin@gmail.com>
2019-06-28 16:57:55 -0400

    renamed, commented servo command code

--- state_reading.py
+++ state_reading.py
@@ -8,10 +8,18 @@ import sys
 mag = serial.Serial('/dev/ttyS6',9600,timeout=2)
 print(mag.name)
 mag.flushInput()
-time.sleep(1)
-fulldata = []
+#time.sleep(1)
 
-while len(fulldata)<10:
+fulldata = []
+"""
+pos = int(sys.argv[1])
+servoMsg = pos.to_bytes(2,'big')
+print(servoMsg)
+servo.write(servoMsg)
+time.sleep(1)
+print(servo.readline())
+"""
+while len(fulldata)<100:
     if(mag.in_waiting):
         raw_bytes = mag.readline()
         decoded_bytes = raw_bytes.decode('utf-8').strip()
@@ -21,7 +29,7 @@ while len(fulldata)<10:
             print(data)
             fulldata.append(data)
 print(sys.argv),
-fname = 'servo_{0}_test_{1}.txt'.format(sys.argv[1],sys.argv[2])
+fname = 'servo_{0}_test_{1}.txt'.format(pos,sys.argv[2])
 print(fname)
 with open(fname,"w") as f:
     for item in fulldata:
