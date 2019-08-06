47b8fccdb4294bf6c8359a0ba53ea9ec3bc89f38
keenechin <keene.chin@gmail.com>
2019-08-06 19:27:43 -0400

    either servo serial control

--- finger_controller/finger_controller.ino
+++ finger_controller/finger_controller.ino
@@ -10,41 +10,36 @@
 #include <ax12.h>
 
 int current = 512;
-int point_one = 512-400;
-int point_two = 512-400;
+int ax_1 = 10;
+int ax_2 = 11;
+int vel_std = 1;
+int incomingByte = 0; // for incoming serial data
+int target = current;
+
 
 void setup()
 {
-    SetPosition(1,1,current); //set the position of servo # 1 to '0'
+    SetPosition(ax_1,current); //set the position of servo # 1 to '0'
+    SetPosition(ax_2,current);
 
     delay(100);//wait for servo to move
     Serial.begin(9600);
+
 }
 
 void loop()
 {
-   
-  /*
-  //increment from 0 to 1023
-  for(int i=0;i<1024;i++)
-  {
-    SetPosition(1,i); //set the position of servo #1 to the current value of 'i'
-    delay(10);//wait for servo to move
+  //int target = 512;
+  while (Serial.available() > 0) {
+    // read the incoming byte:
+    target = Serial.parseInt(); 
   }
-  //decrement from 1024 to 0
-  for(int i=1024;i>0;i--)
-  {
-    SetPosition(1,i);//set the position of servo #1 to the current value of 'i'
-    delay(10);//wait for servo to move
-  }
-  */
+  
+  Serial.println(target);
+  delay(5);
+  goPos(ax_1,target,vel_std);
  
-
-  goPos(point_one,1);
-  //delay(500);
-  goPos(point_two,1);
-  //delay(500);
-
+ 
 }
 
 void goPos(int servo,int pos,int vel){
@@ -53,7 +48,7 @@ void goPos(int servo,int pos,int vel){
       SetPosition(servo,i);
       current = i;
       delay(10);
-      Serial.println(current);
+      //Serial.println(current);
     }
   }
   
@@ -62,7 +57,7 @@ void goPos(int servo,int pos,int vel){
       SetPosition(servo,i);
       current = i;
       delay(10);
-      Serial.println(current);
+      //Serial.println(current);
     }
   }
 }
