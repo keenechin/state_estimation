bd3059a460880c8637ede8d0d820e6baddf72de4
keenechin <keene.chin@gmail.com>
2019-08-14 16:39:59 -0400

    able to read magnetometer and command servo, not synced yet

--- read_state.py
+++ read_state.py
@@ -11,30 +11,29 @@ mag.flushInput()
 #time.sleep(1)
 
 fulldata = []
-"""
-pos = int(sys.argv[1])
-servoMsg = pos.to_bytes(2,'big')
-print(servoMsg)
-servo.write(servoMsg)
-time.sleep(1)
-print(servo.readline())
-"""
 started = False
-while len(fulldata)<100:
+
+def readSerial():
     if(mag.in_waiting):
         raw_bytes = mag.readline()
-        #print(raw_bytes)
         decoded_bytes = raw_bytes.decode('ascii').strip()
-        #print(decoded_bytes)
-
         data = [float(x) for x in decoded_bytes.split()]
-        if data[0]==777:
+        return data
+    else:
+        return [0]
+
+while started == False:
+    data = readSerial()
+    if len(data)==1:
+        if data[0] == 7777777:
             started = True
-        if (len(data)==3 and any(data) and started):
-            print(data)
-            fulldata.append(data)
-        else:
-            print("Not saved: {}".format(data))
+
+while len(fulldata)<100:
+    data = readSerial()
+    if (len(data)==3 and any(data)):
+        print(data)
+        fulldata.append(data)
+
 print(sys.argv),
 fname = 'servo_{0}_test_{1}.txt'.format(sys.argv[1],sys.argv[2])
 print(fname)
--- run_experiment.py
+++ run_experiment.py
@@ -1,6 +1,29 @@
 import serial
 import time
 import sys
+import numpy as np
+
+num_datapoints = 100
+experimental_array = {}
+
+def read_sensor():
+    if(sensor.in_waiting):
+        raw_bytes = sensor.readline()
+        decoded_bytes = raw_bytes.decode('ascii').strip()
+        data = [float(x) for x in decoded_bytes.split()]
+        print(data)
+        return data
+    else:
+        return [0]
+
+def collectStream(stream):
+    while len(stream)<num_datapoints:
+        data = read_sensor()
+        if (len(data)==3 and any(data)):
+            stream.append(data)
+    return stream
+
+
 
 if __name__ == "__main__":
     sensor = serial.Serial('/dev/ttyS6',9600,timeout=2)
@@ -10,13 +33,22 @@ if __name__ == "__main__":
     print("Sensor port: {}".format(sensor.name))
     print("Controller port: {}".format(controller.name))
     sensor.flushInput()
+
+    positions = ["10300","10512","20300","20512"]
+    for target in positions:
+        sensor_stream = []
+        u = "{}".format(target).encode()
+        controller.write(u) 
+        time.sleep(2)
+        sensor_stream = collectStream(sensor_stream)
+        experimental_array[target]=sensor_stream
+
+
+        
+
     
-    while True:
-        if(sensor.in_waiting):
-            raw_bytes = sensor.readline()
-            decoded_bytes = raw_bytes.decode('utf-8').strip()
-            data = [float(x) for x in decoded_bytes.split()]
-            print(data)
+
+    
         
     
     
