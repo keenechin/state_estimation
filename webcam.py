18c10b12be7f21792a36fd4e79432a954e59257f
keenechin <keene.chin@gmail.com>
2019-08-21 17:32:52 -0400

    testing with multiple cams

--- webcam.py
+++ webcam.py
@@ -1,23 +1,34 @@
 #%%
 import cv2
 #%%
-cv2.namedWindow("preview")
-vc = cv2.VideoCapture(2)
+cv2.namedWindow("Camera 1")
+cv2.namedWindow("Camera 2")
+cam_1 = cv2.VideoCapture(0+cv2.CAP_DSHOW)
+cam_2 = cv2.VideoCapture(1+cv2.CAP_DSHOW)
 
-if vc.isOpened():
-    rval, frame = vc.read()
+
+if cam_1.isOpened():
+    val_1, frame_1 = cam_1.read()
 else:
-    rval = False
+    val_1 = False
+
+if cam_2.isOpened():
+    val_2, frame_2 = cam_2.read()
+else:
+    val_2 = False
 
 #%%
-while rval:
-    cv2.imshow("preview", frame)
-    rval, frame = vc.read()
+while val_1 and val_2:
+    cv2.imshow("Camera 1", frame_1)
+    cv2.imshow("Camera 2", frame_2)
+    val_1, frame_1 = cam_1.read()
+    val_2, frame_2 = cam_2.read()
     key = cv2.waitKey(20)
     if key == 27:
         break
 
-cv2.destroyWindow("preview")
+cv2.destroyWindow("Camera 1")
+cv2.destroyWindow("Camera 2")
 
 
 #%%
