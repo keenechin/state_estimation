f37a151d6f1a64161bd53762dee017f9ad134770
keenechin <keene.chin@gmail.com>
2019-08-21 17:33:25 -0400

    incorporated taking pictures into experimental pipeline

--- run_experiment.py
+++ run_experiment.py
@@ -6,9 +6,7 @@ import numpy as np
 import scipy.io as sio
 import cv2
 #%%
-num_datapoints = 100
-experimental_array = np.zeros((1,5))
-save_dict ={}
+
 def read_serial(port,verbose):
     if(port.in_waiting):
         try:
@@ -56,35 +54,80 @@ def serial_setup():
     controller.flushInput()
     return sensor,controller
 
+def camSetup(cam_num):
+    cv2.namedWindow("Cam{}".format(cam_num))
+    cam = cv2.VideoCapture((cam_num-1)+cv2.CAP_DSHOW)
+    if cam.isOpened():
+        val, frame = cam.read()
+    else:
+        val = False
+
+    return cam, val, frame
+
 if __name__ == "__main__":
+    num_datapoints = 100
+    experimental_array = np.zeros((1,5))
+    save_dict ={}
+  
+    cam_1, val_1, frame_1 = camSetup(1)
+    cam_2, val_2, frame_2 = camSetup(2)
+
     sensor,controller = serial_setup()
+
     fname = sys.argv[1]
     with open(fname,'r') as f:
         positions = f.readlines()
+        num_positions = len(positions)
+        stacked_data = np.zeros((num_datapoints,5,num_positions))
+        cam1_frames = np.zeros((num_positions,frame_1.shape[0],frame_1.shape[1],frame_1.shape[2]))
+        cam2_frames = np.zeros_like(cam1_frames)
+
+        pos_idx = 0
         for target in positions:
             target = target[:-1]
             print(target)
-    #positions = ["03000512","04000512","06000512","05120512"]
-    #positions = sys.argv[1:]
-    
             sensor_stream = []
             goPos(target)
-            time.sleep(0.01)
+            time.sleep(0.05)
             sensor.flushInput()
             time.sleep(0.05)
-            sensor_stream = np.array(collectStream(sensor_stream))
+            sensor_stream = np.array(collectStream(sensor_stream))          
+            
+
+            val_1, frame_1 = cam_1.read()          
+            val_2, frame_2 = cam_2.read()
+            if val_1 and val_2:
+                cv2.imshow("Cam1",frame_1)
+                cv2.imshow("Cam2",frame_2)
+            key = cv2.waitKey(20)
+            if key == 27:
+                break
+            time.sleep(0.1)
+            print("{} {}".format(val_1,val_2))
             N = len(sensor_stream)
             ys = np.ones((N,2))
             ys[:,0] = target[:4]
             ys[:,1] = target[4:]
             pos_data = np.hstack((sensor_stream,ys))
             experimental_array = np.vstack((experimental_array, pos_data))
-            save_dict["pos_{}".format(target)] = pos_data
+            stacked_data[:,:,pos_idx] = pos_data
+            cam1_frames[pos_idx,:,:,:] = frame_1
+            cam2_frames[pos_idx,:,:,:] = frame_2
+            #np.append(cam1_frames,frame_1,axis=0)
+            #np.append(cam2_frames,frame_2,axis=0)
+            pos_idx+=1
+
     experimental_array = experimental_array[1:,:]
     print(experimental_array)
     save_dict['data'] = experimental_array
-    sio.savemat("data_{}.mat".format(fname[:-4]), save_dict)
-
+    save_dict['stacked'] = stacked_data
+    save_dict['cam1_frames'] = cam1_frames
+    save_dict['cam2_frames'] = cam2_frames
+    sio.savemat("data_{}.mat".format(fname[2:-4]), save_dict)
+    sensor.close()
+    controller.close()
+    cv2.destroyWindow("Cam1")
+    cv2.destroyWindow("Cam2")
 
         
 
@@ -94,3 +137,6 @@ if __name__ == "__main__":
         
     
     
+
+
+#%%
