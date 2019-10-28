c514220bf7390bb2cbb43f34cca5c81c348d3039
Keene Chin <Kaeladin@users.noreply.github.com>
2019-10-28 17:34:24 -0400

    made matlab regression more streamlined to data we actually use

--- multivariate_regression.m
+++ multivariate_regression.m
@@ -1,4 +1,4 @@
-close all; clear;
+clear;
 %% Get data
 fname = uigetfile("../data/automated/")
 load('-mat',fname);
@@ -52,57 +52,55 @@ X_test_2 = test_data_2(:,1:end-1);
 
 
 %% Train models
-disp('training mimo1')
 [regressor_mimo1, valid_rmse_mimo1] = trainUnivariateRegressionModel([X_train,y_train(:,1)]);%Regenerate these functions using the learner apps, whenever making changes to code
-disp('training mimo2')
 [regressor_mimo2, valid_rmse_mimo2] = trainUnivariateRegressionModel([X_train,y_train(:,2)]);%Regenerate these functions using the learner apps, whenever making changes to code
     %% MIMO1
-disp("MIMO 1:");
+disp("Axis 1:");
 [bias_mimo1,std_mimo1,y_hat_mimo1] = predict(regressor_mimo1,X_test,y_test(:,1));
 %title("Univariate model: Multimodal axis 1 > Multimodal axis 1")
-disp("MIMO 2:");
+disp("Axis 2:");
 [bias_mimo2,std_mimo2,y_hat_mimo2] = predict(regressor_mimo2,X_test,y_test(:,2));
 %title("Univariate model: Multimodal axis 2 > Multimodal axis 2")
 
-%%
-disp('training pipo1')
-[regressor_pipo1, valid_rmse_pipo1] = trainUnivariateRegressionModel(train_data_1);%Regenerate these functions using the learner apps, whenever making changes to code
-disp('training pipo2')
-[regressor_pipo2, valid_rmse_pipo2] = trainUnivariateRegressionModel(train_data_2);%Regenerate these functions using the learner apps, whenever making changes to code
-
-%% MIPO1
-disp("MIPO 1:");
-[bias_mipo1,std_mipo1,y_hat_mipo1] = predict(regressor_mimo1,X_test_1,y_test_1);
-%title("Univariate model: Multimodal axis 1 > Pure axis 1")
-disp("MIPO 2:");
-[bias_mipo2,std_mipo2,y_hat_mipo2] = predict(regressor_mimo2,X_test_2,y_test_2);
-%title("Univariate model: Multimodal axis 2 > Pure axis 2")
-%% PIPO 1
-disp("PIPO 1:");
-[bias_pipo1,std_pipo1,y_hat_pipo1] = predict(regressor_pipo1,X_test_1,y_test_1);
-%title("Univariate model: Pure axis 1 > Pure axis 1")
-disp("PIPO 2:");
-[bias_pipo2,std_pipo2,y_hat_pipo2] = predict(regressor_pipo2,X_test_2,y_test_2);
-%title("Univariate model: Pure axis 2 > Pure axis 2")
-%% PIMO 1
-disp("PIMO 1:");
-[bias_pimo1,std_pimo1,y_hat_pimo1] = predict(regressor_pipo1,X_test,y_test(:,1));
-%title("Univariate model: Pure axis 1 > Multimodal axis 1")
-disp("PIMO 2:");
-[bias_pimo2,std_pimo2,y_hat_pimo2] = predict(regressor_pipo2,X_test,y_test(:,2));
-%title("Univariate model: Pure axis 2 > Multimodal axis 2")
+% %%
+% disp('training pipo1')
+% [regressor_pipo1, valid_rmse_pipo1] = trainUnivariateRegressionModel(train_data_1);%Regenerate these functions using the learner apps, whenever making changes to code
+% disp('training pipo2')
+% [regressor_pipo2, valid_rmse_pipo2] = trainUnivariateRegressionModel(train_data_2);%Regenerate these functions using the learner apps, whenever making changes to code
+% 
+% %% MIPO1
+% disp("MIPO 1:");
+% [bias_mipo1,std_mipo1,y_hat_mipo1] = predict(regressor_mimo1,X_test_1,y_test_1);
+% %title("Univariate model: Multimodal axis 1 > Pure axis 1")
+% disp("MIPO 2:");
+% [bias_mipo2,std_mipo2,y_hat_mipo2] = predict(regressor_mimo2,X_test_2,y_test_2);
+% %title("Univariate model: Multimodal axis 2 > Pure axis 2")
+% %% PIPO 1
+% disp("PIPO 1:");
+% [bias_pipo1,std_pipo1,y_hat_pipo1] = predict(regressor_pipo1,X_test_1,y_test_1);
+% %title("Univariate model: Pure axis 1 > Pure axis 1")
+% disp("PIPO 2:");
+% [bias_pipo2,std_pipo2,y_hat_pipo2] = predict(regressor_pipo2,X_test_2,y_test_2);
+% %title("Univariate model: Pure axis 2 > Pure axis 2")
+% %% PIMO 1
+% disp("PIMO 1:");
+% [bias_pimo1,std_pimo1,y_hat_pimo1] = predict(regressor_pipo1,X_test,y_test(:,1));
+% %title("Univariate model: Pure axis 1 > Multimodal axis 1")
+% disp("PIMO 2:");
+% [bias_pimo2,std_pimo2,y_hat_pimo2] = predict(regressor_pipo2,X_test,y_test(:,2));
+% %title("Univariate model: Pure axis 2 > Multimodal axis 2")
 
 %% Compare
 
-bar_labels = categorical({'pipo', 'mipo', 'mimo', 'pimo'},{'pipo', 'mipo', 'mimo', 'pimo'});
+%bar_labels = categorical({'pipo', 'mipo', 'mimo', 'pimo'},{'pipo', 'mipo', 'mimo', 'pimo'});
 % biases = [bias_pipo1, bias_pipo2; bias_mimo1, bias_mimo2; bias_pimo1, bias_pimo2; bias_mvar1, bias_mvar2; bias_neural1, bias_neural2];
 % figure
 % bar(bar_labels,abs(biases))
-std_deviations = [std_pipo1, std_pipo2; std_mipo1, std_mipo2; std_mimo1, std_mimo2; std_pimo1, std_pimo2];
-figure
-bar(bar_labels,(std_deviations)*99/100)
-title('RMSE across different mappings')
-ylabel('RMSE (servo ticks)')
+%std_deviations = [std_pipo1, std_pipo2; std_mipo1, std_mipo2; std_mimo1, std_mimo2; std_pimo1, std_pimo2];
+% figure
+% bar(bar_labels,(std_deviations)*99/100)
+% title('RMSE across different mappings')
+% ylabel('RMSE (servo ticks)')
 %% Examine Data
 figure
 scatter3(Xs(:,1),Xs(:,2),Xs(:,3),20,ys(:,1),'filled')
