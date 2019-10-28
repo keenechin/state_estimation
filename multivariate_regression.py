5033938fdfe303a5acc34e02cb0c21abfe7dd6b7
Keene Chin <Kaeladin@users.noreply.github.com>
2019-10-28 17:35:42 -0400

    modularized files

--- multivariate_regression.py
+++ multivariate_regression.py
@@ -8,65 +8,68 @@ from sklearn.gaussian_process import GaussianProcessRegressor
 from sklearn.gaussian_process.kernels import Matern
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import r2_score
+from sklearn.metrics import mean_squared_error
 import copy
 
-#%% Read file
 
-def get_data():
-    root = tk.Tk()
-    filename = askopenfilename()
-    print(filename)
-    root.withdraw()
+def get_data(filename):
     mat_contents = sio.loadmat(filename)
     data = mat_contents['data']
     return data, mat_contents
 
-data,_ = get_data()
-
-    #%% Train-test split
-Xs = data[:,1:3]
-ys = data[:,4:5]
-N = len(data)
-train_portion = 0.8
-shuffled = data[np.random.permutation(N),:]
-train_test_cutoff = int(np.floor(N*train_portion))
-train_data = shuffled[0:train_test_cutoff-1,:]
-test_data = shuffled[train_test_cutoff:-1,:]
-y_train = train_data[:,-2:]
-y_test = test_data[:,-2:]
-X_train = train_data[:,:-2]
-X_test = test_data[:,:-2]
-
-
-#%% Evaluate predictions
 def predict(regressor, X_test, y_test):
     y_hat_reg = regressor.predict(X_test)
     errors = y_hat_reg-y_test
     bias = np.mean(errors)
     stdev = np.std(errors)
+    rmse = np.sqrt(mean_squared_error(y_test,y_hat_reg))
     r2 = r2_score(y_test,y_hat_reg)
-    print("Prediction standard deviation: {}".format(stdev))
+    print("Prediction RMSE: {}".format(rmse))
     print("Prediction R^2: {}".format(r2))
+    return [bias,stdev,rmse,r2,y_hat_reg]
 
-    return [bias,stdev,r2,y_hat_reg]
-#%% Init Model
+def partition_data(data):
+    Xs = data[:,1:3]
+    ys = data[:,4:5]
+    N = len(data)
+    train_portion = 0.8
+    np.random.seed(0)
+    shuffled = data[np.random.permutation(N),:]
+    train_test_cutoff = int(np.floor(N*train_portion))
+    train_data = shuffled[0:train_test_cutoff-1,:]
+    test_data = shuffled[train_test_cutoff:-1,:]
+    y_train = train_data[:,-2:]
+    y_test = test_data[:,-2:]
+    X_train = train_data[:,:-2]
+    X_test = test_data[:,:-2]
+    return Xs, ys, X_train, y_train, X_test, y_test
 
-N=20
-d = 6
-regr1 = RandomForestRegressor(max_depth=d,random_state=0,n_estimators=N)
-regr2 = RandomForestRegressor(max_depth=d,random_state=0,n_estimators=N)
-#%% Train Model
-print("N: {}, d: {}".format(N,d))
-print("Training mimo 1...")
-regr1.fit(X_train,y_train[:,0].ravel())
-bias_mimo1,std_mimo1,r2_1,y_hat_mimo1 = predict(regr1,X_test,y_test[:,0])
+def get_trained_models(X_train, y_train, N,d):
+    regr1 = RandomForestRegressor(max_depth=d,random_state=0,n_estimators=N)
+    regr2 = RandomForestRegressor(max_depth=d,random_state=0,n_estimators=N)
+    print("N: {}, d: {}".format(N,d))
+    print("Training mimo 1...")
+    regr1.fit(X_train,y_train[:,0].ravel())
+    print("Training mimo 2...")
+    regr2.fit(X_train,y_train[:,1].ravel())
+    return regr1, regr2
 
-print("Training mimo 2...")
-regr2.fit(X_train,y_train[:,1].ravel())
 
-bias_mimo2,std_mimo2,r2_2,y_hat_mimo2 = predict(regr2,X_test,y_test[:,1])
-print("")
-#%%
-from joblib import dump,load
-dump(regr1,'regr1.joblib')
-dump(regr2,'regr2.joblib')
+if __name__ == "__main__":
+    root = tk.Tk()
+    filename = askopenfilename()
+    print(filename)
+    root.withdraw()
+    data,_ = get_data(filename)
+    Xs, ys, X_train, y_train, X_test, y_test = partition_data(data)
+    N = 40
+    d = 6
+    regr1, regr2 = get_trained_models(X_train,y_train,N,d)
+
+
+    bias_mimo1,std_mimo1,rmse1,r2_1,y_hat_mimo1 = predict(regr1,X_test,y_test[:,0])
+    bias_mimo2,std_mimo2,r2_2,rmse2,y_hat_mimo2 = predict(regr2,X_test,y_test[:,1])
+    print("")    #%% save models
+    from joblib import dump,load
+    dump(regr1,'regr1.joblib')
+    dump(regr2,'regr2.joblib')
--- run_experiment.py
+++ run_experiment.py
@@ -7,13 +7,18 @@ import scipy.io as sio
 import cv2
 from serial_handler import read_serial,serial_setup, goPos
 import numpy.matlib
+import tkinter as tk
+from tkinter import filedialog 
+from tkinter import Tk
+import os
+import pandas as pd
 #%%
 
 
 
 def collectStream(stream):
     while len(stream)<num_datapoints:
-        data = read_serial(sensor,True)
+        data = read_serial(sensor,False)
         if (len(data)==3 and any(data)):
             stream.append(data)
     return stream
@@ -31,70 +36,85 @@ def camSetup(cam_num):
     return cam, val, frame
 
 if __name__ == "__main__":
-    num_datapoints = int(sys.argv[2])
-    stime = float(sys.argv[3])
-    experimental_array = np.zeros((1,5))
-    save_dict ={}
-  
+    num_datapoints = int(sys.argv[1])
+    stime = float(sys.argv[2])
+    root = tk.Tk()
+    root.withdraw()
+    folder = filedialog.askdirectory()
+    if len(sys.argv) == 4:
+        files = sys.argv[3]
+    else:
+        files = os.listdir(folder)
     cam_1, val_1, frame_1 = camSetup(1)
-    
+
 
     for i in range(0,100):#give camera a chance to autoadjust brightness
             val_1, frame_1 = cam_1.read()
             key = cv2.waitKey(20)
             if key == 27:
                 break
-
     sensor,controller = serial_setup()
+    experiment_progress = 0
+    num_files = len(files)
+    for test_file in files:
+        fname = "{}/{}".format(folder,test_file)
+        print(fname)
+        outfile = fname[1+len(folder):-4]
 
-    fname = sys.argv[1]
-    with open(fname,'r') as f:
-        positions = f.readlines()
-        print(positions)
-        num_positions = len(positions)
-        stacked_data = np.zeros((num_datapoints,5,num_positions))
-        cam1_frames = np.zeros((num_positions,frame_1.shape[0],frame_1.shape[1],frame_1.shape[2]))
-        val_1, frame_1 = cam_1.read()          
+        experimental_array = np.zeros((1,5))
+        save_dict ={}
+    
 
-        experiment_progress = 0
 
-        pos_idx = 0
-        for target in positions:
-            key = cv2.waitKey(20)
-            if key == 27:
-                break
-            target = target[:-1]
-            print(target)
-            print("{:5.2f}%% complete".format(experiment_progress*100/num_positions))
-            experiment_progress+=1
-            sensor_stream = []
-            goPos(controller,target)
-            time.sleep(stime)
-            sensor.reset_input_buffer()
-            time.sleep(0.01)
-            sensor_stream = np.array(collectStream(sensor_stream))   
-            for i in range(0,3):
-                val_1, frame_1 = cam_1.read()
-            time.sleep(0.1)
-            N = len(sensor_stream)
-            ys = np.ones((N,2))
-            ys[:,0] = target[:4]
-            ys[:,1] = target[4:]
-            pos_data = np.hstack((sensor_stream,ys))
-            experimental_array = np.vstack((experimental_array, pos_data))
-            stacked_data[:,:,pos_idx] = pos_data
-            cam1_frames[pos_idx,:,:,:] = frame_1
-            pos_idx+=1
+        with open(fname,'r') as f:
+            positions = f.readlines()
+            num_positions = len(positions)
+            stacked_data = np.zeros((num_datapoints,5,num_positions))
+            cam1_frames = np.zeros((num_positions,frame_1.shape[0],frame_1.shape[1],frame_1.shape[2]))
+            val_1, frame_1 = cam_1.read()          
 
-    experimental_array = experimental_array[1:,:]
-    print(experimental_array)
-    save_dict['data'] = experimental_array
-    save_dict['stacked'] = stacked_data
-    save_dict['cam1_frames'] = cam1_frames
-    sio.savemat("../data/automated/sleep{}_{}_n_{}.mat".format(stime,fname[2:-4],num_datapoints), save_dict)
-    sensor.close()
-    controller.close()
-    cv2.destroyWindow("Cam1")
+
+            pos_idx = 0
+            for target in positions:
+                key = cv2.waitKey(20)
+                if key == 27:
+                    break
+                target = target[:-1]
+                print(target)
+                sensor_stream = []
+                goPos(controller,target)
+                time.sleep(stime)
+                sensor.reset_input_buffer()
+                time.sleep(0.01)
+                sensor_stream = np.array(collectStream(sensor_stream))   
+                for i in range(0,3):
+                    val_1, frame_1 = cam_1.read()
+                time.sleep(0.1)
+                N = len(sensor_stream)
+                ys = np.ones((N,2))
+                ys[:,0] = target[:4]
+                ys[:,1] = target[4:]
+                pos_data = np.hstack((sensor_stream,ys))
+                experimental_array = np.vstack((experimental_array, pos_data))
+                stacked_data[:,:,pos_idx] = pos_data
+                cam1_frames[pos_idx,:,:,:] = frame_1
+                pos_idx+=1
+                experiment_progress+=1
+                print("{:5.2f}%% complete".format(experiment_progress*100/(num_files*num_positions)))
+
+
+        experimental_array = experimental_array[1:,:]
+        print(experimental_array)
+        save_dict['data'] = experimental_array
+        save_dict['stacked'] = stacked_data
+        save_dict['cam1_frames'] = cam1_frames
+        
+        sio.savemat("../data/automated/{1}_sleep{0}_n_{2}.mat".format(int(stime),outfile,num_datapoints), save_dict)
+        time.sleep(1)
+
+sensor.close()
+controller.close()
+cv2.destroyWindow("Cam1")
 
         
 
